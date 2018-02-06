1c864e05564ab5b9cbe23733a66177277e5088f8
赵 海鑫 <zhaokaixin@MacBook-Pro.local>
2018-02-06 14:28:10 +0800

    爬虫测试

--- 个人使用/测试.py
+++ 个人使用/测试.py
@@ -1,21 +1,9 @@
-#-*- coding: UTF-8 -*- 。
-#检索关键词
-#平台MT6276
-import os
-import zipfile
-def zip_dir(dirname,zipfilename):
-    filelist = []
-    if os.path.isfile(dirname):
-        filelist.append(dirname)
-    else :
-        for root, dirs, files in os.walk(dirname):
-            for name in files:
-                filelist.append(os.path.join(root, name))
-         
-    zf = zipfile.ZipFile(zipfilename, "w", zipfile.zlib.DEFLATED)
-    for tar in filelist:
-        arcname = tar[len(dirname):]
-        #print arcname
-        zf.write(tar,arcname)
-    zf.close()
-zip_dir(r'/home/zhaohaixin/桌面/修改',r'/home/zhaohaixin/桌面/xiugai.zip')
+#!/usr/bin/env python3
+import sys
+import urllib.request
+from bs4 import BeautifulSoup
+page = urllib.request.urlopen('https://www.cnblogs.com/Axi8/p/5757270.html')#打开网页
+#htmlcode = page.read()#读取页面源码
+htmlcode=BeautifulSoup(page)
+print (sys.version)
+print (htmlcode)#在控制台输
